20367f8338121666de6727846e37a37e3eb3bf8b
AstewaleT <astewaletsega20@gmail.com>
2024-02-22 01:40:14 +0300

    common css

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
-
+// console.log('Initial Store State:', store.getState());
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
